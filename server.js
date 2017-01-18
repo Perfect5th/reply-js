8fa9a027406c3a9cccbf30f4984cf14b4cecca85
perfect5th <mitch.b45@gmail.com>
2017-01-18 05:05:56 +0000

    Add simple response handling

--- server.js
+++ server.js
@@ -5,11 +5,30 @@ http.createServer(function(request, response) {
   var method = request.method;
   var url = request.url;
   var body = [];
+
   request.on('error', function(err) {
     console.error(err);
   }).on('data', function(chunk) {
     body.push(chunk);
   }).on('end', function() {
     body = Buffer.concat(body).toString();
+
+    // start of response
+    response.on('error', function(err) {
+      console.error(err);
+    });
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      body: body
+    };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
   });
 }).listen(8080);
