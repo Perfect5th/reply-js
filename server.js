656c2e67d04fc2cb473fd933f2dbfa6f189c7bcf
perfect5th <mitch.b45@gmail.com>
2017-01-18 04:56:36 +0000

    Update VagrantFile for port-forwarding Add simple server

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+  });
+}).listen(8080);
